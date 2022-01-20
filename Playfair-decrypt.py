f5755d3302e0b2e0cb987fc243aafa6e9d306f86
joann <s107321004@mail1.ncnu.edu.tw>
2022-01-20 23:38:53 +0800

    modify files

--- Playfair-decrypt.py
+++ Playfair-decrypt.py
@@ -3,44 +3,21 @@
 # rule: add X in the end of the plaintext if the lenghth is odd
 import sys
 import string
-len_argv = len(sys.argv)
 
-key = ""
-for i in range(1, len_argv):
-	key += sys.argv[i]
-key = key.translate(str.maketrans('', '', string.punctuation)).upper()
-key = key.replace(" ", "")
-len_key = len(key)
+def generate_table(key):
+    # create a 5x5 table (replace IJ with II)
+    table = "ABCDEFGHIIKLMNOPQRSTUVWXYZ"
+    for i in range(0, len(table)):
+        if table[i] not in key:
+            key += str(table[i])
 
-# replace J/I with I/J
-for i in range(len_key):
-	if key[i] == "J":
-		key[i] = "I"
-
-key = ''.join(sorted(set(key), key=key.index))
-
-# create a 5x5 table (replace IJ with II)
-table = "ABCDEFGHIIKLMNOPQRSTUVWXYZ"
-for i in range(0, len(table)):
-    if table[i] not in key:
-        key += str(table[i])
-
-maze = [[0 for x in range(5)] for y in range(5)]
-index = 0
-for i in range(0, 5):
-    for j in range(0, 5):
-        maze[i][j] = key[index]
-        index += 1
-
-#ciphertext = input("ciphertext: ")
-ciphertext = ""
-while True:
-    try:
-        n = input().replace("\n", "")
-        ciphertext += n
-    except EOFError:
-        break
-ciphertext = ciphertext.upper().replace(" ", "")
+    maze = [[0 for x in range(5)] for y in range(5)]
+    index = 0
+    for i in range(0, 5):
+        for j in range(0, 5):
+            maze[i][j] = key[index]
+            index += 1
+    return maze
 
 def separate(ciphertext):
     matrix = []
@@ -50,7 +27,7 @@ def separate(ciphertext):
         i += 2
     return matrix
 
-def findlocation(c):
+def findlocation(c, maze):
     location = list()
     if c == 'J':
         c = 'I'
@@ -61,27 +38,57 @@ def findlocation(c):
                 location.append(k)
     return location
 
-matrix = separate(ciphertext)
-i = 0
-plaintext = ""
-while i < len(matrix):
-    loc_1 = findlocation(matrix[i][0])
-    loc_2 = findlocation(matrix[i][1])
-    if loc_1[1] == loc_2[1]:	# same row
-        plaintext += (maze[(loc_1[0]-1+5)%5][loc_1[1]] + maze[(loc_2[0]-1+5)%5][loc_2[1]])
-    elif loc_1[0] == loc_2[0]:	# same column
-        plaintext += (maze[loc_1[0]][(loc_1[1]-1+5)%5] + maze[loc_2[0]][(loc_2[1]-1+5)%5])
-    else:
-        plaintext += (maze[loc_1[0]][loc_2[1]] + maze[loc_2[0]][loc_1[1]])
-    i += 1
+def main():
+    len_argv = len(sys.argv)
 
-# couple with double alphabet and the end X
-length = len(plaintext)
-if length % 2 == 0 and plaintext[length-1] == "X":
-	plaintext = plaintext[:-1]
-length = len(plaintext)
-for i in range(0, length-3):
-	if plaintext[i] == plaintext[i+2] and plaintext[i+1] == "X":
-		plaintext = plaintext[:i+1] + plaintext[i+2:]
+    key = ""
+    for i in range(1, len_argv):
+        key += sys.argv[i]
+    key = key.translate(str.maketrans('', '', string.punctuation)).upper()
+    key = key.replace(" ", "")
+    len_key = len(key)
 
-print("pliantext: ", plaintext)
+    # replace J/I with I/J
+    for i in range(len_key):
+        if key[i] == "J":
+            key[i] = "I"
+
+    key = ''.join(sorted(set(key), key=key.index))
+    maze = generate_table(key)
+
+    #ciphertext = input("ciphertext: ")
+    ciphertext = ""
+    while True:
+        try:
+            n = input().replace("\n", "")
+            ciphertext += n
+        except EOFError:
+            break
+    ciphertext = ciphertext.upper().replace(" ", "")
+
+    matrix = separate(ciphertext)
+    i = 0
+    plaintext = ""
+    while i < len(matrix):
+        loc_1 = findlocation(matrix[i][0], maze)
+        loc_2 = findlocation(matrix[i][1], maze)
+        if loc_1[1] == loc_2[1]:	# same row
+            plaintext += (maze[(loc_1[0]-1+5)%5][loc_1[1]] + maze[(loc_2[0]-1+5)%5][loc_2[1]])
+        elif loc_1[0] == loc_2[0]:	# same column
+            plaintext += (maze[loc_1[0]][(loc_1[1]-1+5)%5] + maze[loc_2[0]][(loc_2[1]-1+5)%5])
+        else:
+            plaintext += (maze[loc_1[0]][loc_2[1]] + maze[loc_2[0]][loc_1[1]])
+        i += 1
+
+    # couple with double alphabet and the end X
+    length = len(plaintext)
+    if length % 2 == 0 and plaintext[length-1] == "X":
+    	plaintext = plaintext[:-1]
+    length = len(plaintext)
+    for i in range(0, length-3):
+    	if plaintext[i] == plaintext[i+2] and plaintext[i+1] == "X":
+    		plaintext = plaintext[:i+1] + plaintext[i+2:]
+
+    print("pliantext: ", plaintext)
+
+main()
--- Playfair-encrypt.py
+++ Playfair-encrypt.py
@@ -3,54 +3,21 @@
 # rule: add X in the end of the plaintext if the lenghth is odd
 import sys
 import string
-len_argv = len(sys.argv)
 
-key = ""
-for i in range(1, len_argv):
-	key += sys.argv[i]
-key = key.translate(str.maketrans('', '', string.punctuation)).upper()
-key = key.replace(" ", "")
-len_key = len(key)
+def generate_table(key):
+    # create a 5x5 table (replace IJ with II)
+    table = "ABCDEFGHIIKLMNOPQRSTUVWXYZ"
+    for i in range(0, len(table)):
+        if table[i] not in key:
+            key += str(table[i])
 
-# replace J/I with I/J
-for i in range(len_key):
-	if key[i] == "J":
-		key[i] = "I"
-
-key = ''.join(sorted(set(key), key=key.index))
-
-# create a 5x5 table (replace IJ with II)
-table = "ABCDEFGHIIKLMNOPQRSTUVWXYZ"
-for i in range(0, len(table)):
-    if table[i] not in key:
-        key += str(table[i])
-
-maze = [[0 for x in range(5)] for y in range(5)]
-index = 0
-for i in range(0, 5):
-    for j in range(0, 5):
-        maze[i][j] = key[index]
-        index += 1
-
-#plaintext = input("plaintext: ")
-plaintext = ""
-while True:
-    try:
-        n = input().replace("\n", "")
-        plaintext += n
-    except EOFError:
-        break
-
-plaintext = plaintext.upper().replace(" ", "")
-length = len(plaintext)
-# couple with double alphabet
-for i in range(0, length-1):
-	if plaintext[i] == plaintext[i+1]:
-		plaintext = plaintext[:i+1] + 'X' + plaintext[i+1:]
-
-length = len(plaintext)
-if length % 2 == 1:
-	plaintext += "X"
+    maze = [[0 for x in range(5)] for y in range(5)]
+    index = 0
+    for i in range(0, 5):
+        for j in range(0, 5):
+            maze[i][j] = key[index]
+            index += 1
+    return maze
 
 def separate(plaintext):
     matrix = []
@@ -60,7 +27,7 @@ def separate(plaintext):
         i += 2
     return matrix
 
-def findlocation(c):
+def findlocation(c, maze):
     location = list()
     if c == 'J':
         c = 'I'
@@ -71,18 +38,57 @@ def findlocation(c):
                 location.append(k)
     return location
 
-matrix = separate(plaintext)
-i = 0
-ciphertext = ""
-while i < len(matrix):
-    loc_1 = findlocation(matrix[i][0])
-    loc_2 = findlocation(matrix[i][1])
-    if loc_1[1] == loc_2[1]:	# same row
-        ciphertext += (maze[(loc_1[0]+1)%5][loc_1[1]] + maze[(loc_2[0]+1)%5][loc_2[1]])
-    elif loc_1[0] == loc_2[0]:	# same column
-        ciphertext += (maze[loc_1[0]][(loc_1[1]+1)%5] + maze[loc_2[0]][(loc_2[1]+1)%5])
-    else:
-        ciphertext += (maze[loc_1[0]][loc_2[1]] + maze[loc_2[0]][loc_1[1]])
-    i += 1
+def main():
+    len_argv = len(sys.argv)
 
-print("ciphertext: ", ciphertext)
+    key = ""
+    for i in range(1, len_argv):
+        key += sys.argv[i]
+    key = key.translate(str.maketrans('', '', string.punctuation)).upper()
+    key = key.replace(" ", "")
+    len_key = len(key)
+
+    # replace J/I with I/J
+    for i in range(len_key):
+        if key[i] == "J":
+            key[i] = "I"
+
+    key = ''.join(sorted(set(key), key=key.index))
+    maze = generate_table(key)
+    #plaintext = input("plaintext: ")
+    plaintext = ""
+    while True:
+        try:
+            n = input().replace("\n", "")
+            plaintext += n
+        except EOFError:
+            break
+
+    plaintext = plaintext.upper().replace(" ", "")
+    length = len(plaintext)
+    # couple with double alphabet
+    for i in range(0, length-1):
+        if plaintext[i] == plaintext[i+1]:
+            plaintext = plaintext[:i+1] + 'X' + plaintext[i+1:]
+
+    length = len(plaintext)
+    if length % 2 == 1:
+        plaintext += "X"
+
+    matrix = separate(plaintext)
+    i = 0
+    ciphertext = ""
+    while i < len(matrix):
+        loc_1 = findlocation(matrix[i][0], maze)
+        loc_2 = findlocation(matrix[i][1], maze)
+        if loc_1[1] == loc_2[1]:	# same row
+            ciphertext += (maze[(loc_1[0]+1)%5][loc_1[1]] + maze[(loc_2[0]+1)%5][loc_2[1]])
+        elif loc_1[0] == loc_2[0]:	# same column
+            ciphertext += (maze[loc_1[0]][(loc_1[1]+1)%5] + maze[loc_2[0]][(loc_2[1]+1)%5])
+        else:
+            ciphertext += (maze[loc_1[0]][loc_2[1]] + maze[loc_2[0]][loc_1[1]])
+        i += 1
+
+    print("ciphertext: ", ciphertext)
+
+main()
